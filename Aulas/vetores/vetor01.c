fc3b077af1089fbd47b74ffca38dfe1c3e8b0f7b
Jessé Augusto <jessealvessilva@gmail.com>
2023-10-21 15:30:17 -0300

    Exemplos de vetor

---
+++ Aulas/vetores/vetor01.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	// Declaração
+	int vetor[5]; 	
+	
+	// Atribuição de valor
+	vetor[0]  =  10;
+	vetor[1]  =  55;
+	vetor [2] =  200;
+	vetor [3] =  35;
+	vetor [4] =  2;
+    
+    printf("\n\nValue of the position is 0 equal is %d ", vetor[0] );
+    
+    printf("\nValue of the position is 1 equal is %d ", vetor[1] );
+    
+ 	printf("\nValue of the position is 2 equal is %d ", vetor[2] );    
+ 	
+ 	printf("\nValue of the position is 3 equal is %d ", vetor[3] );    
+ 	
+ 	printf("\nValue of the position is 4 equal is %d ", vetor[4] );    
+	
+	printf("\n\n\n\n");
+	return 0;
+}
